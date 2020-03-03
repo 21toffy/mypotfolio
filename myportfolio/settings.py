4be0619f15c060bec402963fe3d8029417129b91
toffy <oketofoke@gmail.com>
2020-03-03 02:10:54 +0100

    worked on static files

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -115,13 +115,13 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+# STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
     ]
 
-# STATIC_ROOT = os.path.join(BASE_DIR, "root")
+STATIC_ROOT = os.path.join(BASE_DIR, "root")
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
