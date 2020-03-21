1fb1cdf31c10f86c88ac6fab7a6afa5990b9dad8
toffy <oketofoke@gmail.com>
2020-03-21 02:25:10 +0100

    removed trailing slash from media root path in settings.py, maybe media files would be serverd now

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -120,7 +120,13 @@ STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 CRISPY_TEMPLATE_PACK='bootstrap4'
+
+
+
+
+
+
