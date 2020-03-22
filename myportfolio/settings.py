378180c203f1c496480d9acb8021e2fa61a77251
toffy <oketofoke@gmail.com>
2020-03-22 22:34:23 +0100

    edted settings to contain new ip and added the media serving url to the urls.py

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = '^e1gmgbq17c6$4$h3mc0@crr(2^*937$6(*v0u#@l&*rebjg7a'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ec2-3-15-194-168.us-east-2.compute.amazonaws.com', '3.15.194.168', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['ec2-3-12-103-252.us-east-2.compute.amazonaws.com', '3.15.194.168', '127.0.0.1', 'localhost']
 #for this
 
 # Application definition
