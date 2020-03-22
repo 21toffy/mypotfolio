a9ca1e582ddaa28549cffe1a608ffa84b99627b3
toffy <oketofoke@gmail.com>
2020-03-22 23:16:36 +0100

    removed enviroment variables from wsgi.py

--- myportfolio/wsgi.py
+++ myportfolio/wsgi.py
@@ -6,12 +6,10 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
-import os
-from dotenv import load_dotenv
-from django.core.wsgi import get_wsgi_application
-project_folder = os.path.expanduser('~/mypotfolio')  # adjust as appropriate
-load_dotenv(os.path.join(project_folder, '.env'))
 
+import os
+
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myportfolio.settings")
 
