e4a72d9d04051afd74449bca6c9db8013af8754f
toffy <oketofoke@gmail.com>
2020-06-22 03:54:36 +0100

    removed some dependencies decouple and all

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -1,9 +1,5 @@
 
 import os
-from decouple import config, Csv
-
-
-import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
